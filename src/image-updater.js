2534f2f1eab2e98099ad40fc7024ca791aa2aa79
Alem <paulo.alem@willbank.com.br>
2023-09-26 16:28:55 -0300

    test

--- src/image-updater.js
+++ src/image-updater.js
@@ -2,16 +2,10 @@ const yamlChanger = require("./utils/yaml-changer")
 const gitHandler = require("./utils/git-handler")
 
 async function updateImageTag(newImageTag, repository_name) {
+    
     console.info("[Info]:: Running the image update")
     
-    //Github repository name
-    //Verificar se tem  arquivo deployment, tem deployment? então é deployment se não é cronjob.
-
-    
-    // folderPath= `${environment}/${repository_name}/deployment.yaml`
-    
     const folderPath= `dev/xpto-api/`
-    const key = "newTag"
 
     yamlChanger.insertYamlValue(folderPath, newImageTag)
 }
