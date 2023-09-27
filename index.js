158e8350956b79c42ba9ea667ec1e79924259eb6
Alem <paulo.alem@willbank.com.br>
2023-09-27 11:26:32 -0300

    test

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ var githubSHA = process.env.GITHUB_SHA
 var githubActor = process.env.GITHUB_ACTOR
 var githubRepository = process.env.GITHUB_REPOSITORY
 
+var repositoryName = githubRepository.split("/")[1]
+
+
 async function main() {
     try {
         const environment = core.getInput("environment").toString()
 
-        await imageUpdater.updateImageTag(githubSHA, githubRepository, environment)
+        await imageUpdater.updateImageTag(githubSHA, repositoryName, environment)
         await imageUpdater.commitNewImageTag(githubSHA, githubActor)
 
     } catch (error) {
