2a3b8a54e3126180c3903b8cdb696d27dfc12d67
Alem <paulo.alem@willbank.com.br>
2023-10-10 10:05:35 -0300

    add tag input

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const core = require("@actions/core")
 const configutator = require("./src/utils/configurator")
 const imageUpdater = require("./src/image-updater")
 
-var githubSHA = process.env.GITHUB_SHA
 var githubActor = process.env.GITHUB_ACTOR
 var githubRepository = process.env.GITHUB_REPOSITORY
 
@@ -12,16 +11,13 @@ var repositoryName = githubRepository.split("/")[1]
 async function main() {
     try {
         var environment = core.getInput("environment").toString()
-        console.info("environment: " + environment)
-
         if (environment == "") {
             environment = "dev";
         }
+        var tag = core.getInput("tag").toString()
 
-        console.info("environment: " + environment)
-
-        await imageUpdater.updateImageTag(githubSHA, repositoryName, environment)
-        await imageUpdater.commitNewImageTag(githubSHA, githubActor, repositoryName, environment)
+        await imageUpdater.updateImageTag(tag, repositoryName, environment)
+        await imageUpdater.commitNewImageTag(tag, githubActor, repositoryName, environment)
 
     } catch (error) {
         throw Error(error)
