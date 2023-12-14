e78971748c04ad24d682d883a0fc8dea57eef734
Ramon Almeida <ramon.almeida@snap.fi>
2023-12-14 15:04:47 -0300

    fix: reading cronjob spec

--- src/utils/yaml-changer.js
+++ src/utils/yaml-changer.js
@@ -1,21 +1,26 @@
-const fs = require('fs');
-const path = require('path');
-const yaml = require('js-yaml');
+const fs = require("fs");
+const path = require("path");
+const yaml = require("js-yaml");
 
 function getResourceNameFromFilename(filePath) {
   const fileName = path.basename(filePath);
-  if (fileName === 'deployment.yaml') {
-    return 'deployment';
-  } else if (fileName === 'cronjob.yaml') {
-    return 'cronjob';
+  if (fileName === "deployment.yaml") {
+    return "deployment";
+  } else if (fileName === "cronjob.yaml") {
+    return "cronjob";
   } else {
-    return '';
+    return "";
   }
 }
 
 function insertYamlValue(folderPath, newImageTag) {
   if (!folderPath || !newImageTag) {
-    console.error("Please provide a folder path and a new image tag as arguments - folder path: " + folderPath + " tag: " + tag);
+    console.error(
+      "Please provide a folder path and a new image tag as arguments - folder path: " +
+        folderPath +
+        " tag: " +
+        tag
+    );
     process.exit(1);
   }
 
@@ -28,7 +33,7 @@ function insertYamlValue(folderPath, newImageTag) {
         const stat = fs.statSync(filePath);
         if (stat.isDirectory()) {
           files.push(...findYamlFiles(filePath));
-        } else if (file === 'deployment.yaml' || file === 'cronjob.yaml') {
+        } else if (file === "deployment.yaml" || file === "cronjob.yaml") {
           files.push(filePath);
         }
       });
@@ -38,13 +43,15 @@ function insertYamlValue(folderPath, newImageTag) {
     const yamlFiles = findYamlFiles(folderPath);
 
     if (yamlFiles.length === 0) {
-      console.error('No deployment.yaml or cronjob.yaml files found in the specified folder.');
+      console.error(
+        "No deployment.yaml or cronjob.yaml files found in the specified folder."
+      );
       process.exit(1);
     }
 
     // Iterate through the found YAML files
     for (const yamlFilePath of yamlFiles) {
-      const yamlContent = fs.readFileSync(yamlFilePath, 'utf8');
+      const yamlContent = fs.readFileSync(yamlFilePath, "utf8");
       const resourceConfig = yaml.load(yamlContent);
 
       if (!resourceConfig) {
@@ -55,29 +62,42 @@ function insertYamlValue(folderPath, newImageTag) {
       const resourceName = getResourceNameFromFilename(yamlFilePath);
 
       // Update the image tag based on the resource type
-      if (resourceName === 'deployment' || resourceName === 'cronjob') {
-        const spec = resourceConfig.spec || resourceConfig.jobTemplate.spec;
-        if (spec && spec.template && spec.template.spec && spec.template.spec.containers) {
+      if (resourceName === "deployment" || resourceName === "cronjob") {
+        const spec = resourceConfig.spec.jobTemplate
+          ? resourceConfig.spec.jobTemplate.spec
+          : resourceConfig.spec;
+        if (
+          spec &&
+          spec.template &&
+          spec.template.spec &&
+          spec.template.spec.containers
+        ) {
           spec.template.spec.containers.forEach((container) => {
-            container.image = `${container.image.split(':')[0]}:${newImageTag}`;
+            container.image = `${container.image.split(":")[0]}:${newImageTag}`;
           });
 
           // Convert the updated configuration back to YAML
           const updatedYaml = yaml.dump(resourceConfig);
 
           // Write the updated YAML back to the file
-          fs.writeFileSync(yamlFilePath, updatedYaml, 'utf8');
+          fs.writeFileSync(yamlFilePath, updatedYaml, "utf8");
 
-          console.log(`Image tag for ${resourceName} in ${yamlFilePath} updated successfully.`);
+          console.log(
+            `Image tag for ${resourceName} in ${yamlFilePath} updated successfully.`
+          );
         } else {
-          console.error(`Failed to update image tag in ${yamlFilePath}. ${resourceName} configuration is invalid.`);
+          console.error(
+            `Failed to update image tag in ${yamlFilePath}. ${resourceName} configuration is invalid.`
+          );
         }
       } else {
-        console.error(`Unsupported resource type in ${yamlFilePath}: ${resourceName}`);
+        console.error(
+          `Unsupported resource type in ${yamlFilePath}: ${resourceName}`
+        );
       }
     }
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
     process.exit(1);
   }
 }
